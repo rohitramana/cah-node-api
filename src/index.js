bc6d5ee4a6caa9c5d90aa8764a3ddcceb676600d
Rohit Seetepalli <rohitset@amazon.com>
2020-03-03 14:49:29 -0800

    serving static file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import http from 'http';
 import url from 'url';
 import { questions, answers } from './cards';
 
+var nStatic = require('node-static');
+var fileServer = new nStatic.Server('./public');
+
 const { floor: floor, random: random } = Math;
 const nquestions = questions.length, nanswers = answers.length;
 const question = () => { return questions[floor(random()*nquestions)] };
@@ -18,6 +21,8 @@ http.createServer((req, res) => {
     res.write(answer());
   else if (path === '/pick')
     res.write(JSON.stringify(pick()));
+  else if (path === '/loaderio-1212ad9a809c470d05da15ff7dea105c')
+    fileServer.serve(req, res);
   else
     res.write(`USAGE:
                /question - get a random white card
