fc0b9ae006a942cb2fce3e3104397c4e160c471e
Rohit Seetepalli <rohitset@amazon.com>
2020-03-04 12:14:10 -0800

    Final try 10

--- src/index.js
+++ src/index.js
@@ -33,3 +33,10 @@
 
 // }).listen(parseInt(process.env.PORT) || 5000);
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
